7ed62917f2bc0209ea952bc1e13aadb725dad499
chenshenchao <chenshenchao@outlook.com>
2024-02-24 01:09:09 +0800

    SAVE

---
+++ hijdemo/winmm/src/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <Windows.h>
+
+#define HIJ_API extern "C" __declspec(dllexport)
+
+BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved) {
+    TCHAR systemDir[MAX_PATH] = { 0 };
+    switch (reason) {
+    case DLL_PROCESS_ATTACH:
+        MessageBox(NULL, TEXT("I'm winmm.dll"), "Info", MB_OK);
+        break;
+    }
+
+    return TRUE;
+}
+
+HIJ_API DWORD timeGetTime() {
+    return 0;
+}
