d375d4b0e1ed0a4dac018cb0e1b1192cf7934bfb
chenshenchao <chenshenchao@outlook.com>
2024-02-26 02:12:29 +0800

    save

--- hijdemo/wininet/src/main.cpp
+++ hijdemo/wininet/src/main.cpp
@@ -50,6 +50,76 @@ typedef HINTERNET (*FnPtrInternetConnectW)(
     DWORD dwService,
     DWORD dwFlags,
     DWORD_PTR dwContext);
+typedef BOOL (*FnPtrHttpQueryInfoW)(
+    HINTERNET hRequest,
+    DWORD     dwInfoLevel,
+    LPVOID    lpBuffer,
+    LPDWORD   lpdwBufferLength,
+    LPDWORD   lpdwIndex
+);
+typedef BOOL (*FnPtrHttpAddRequestHeadersW)(
+    HINTERNET hRequest,
+    LPCWSTR   lpszHeaders,
+    DWORD     dwHeadersLength,
+    DWORD     dwModifiers
+);
+typedef BOOL(*FnPtrHttpSendRequestW)(
+     HINTERNET hRequest,
+     LPCWSTR   lpszHeaders,
+     DWORD     dwHeadersLength,
+     LPVOID    lpOptional,
+     DWORD     dwOptionalLength
+);
+typedef BOOL(*FnPtrInternetSetOptionW)(
+     HINTERNET hInternet,
+     DWORD     dwOption,
+     LPVOID    lpBuffer,
+    DWORD     dwBufferLength
+);
+typedef BOOL(*FnPtrHttpQueryInfoA)(
+         HINTERNET hRequest,
+          DWORD     dwInfoLevel,
+     LPVOID    lpBuffer,
+     LPDWORD   lpdwBufferLength,
+    LPDWORD   lpdwIndex
+);
+typedef HINTERNET(*FnPtrHttpOpenRequestW)(
+    HINTERNET hConnect,
+     LPCWSTR   lpszVerb,
+     LPCWSTR   lpszObjectName,
+     LPCWSTR   lpszVersion,
+     LPCWSTR   lpszReferrer,
+     LPCWSTR* lplpszAcceptTypes,
+     DWORD     dwFlags,
+     DWORD_PTR dwContext
+);
+typedef BOOL(*FnPtrInternetWriteFile)(
+      HINTERNET hFile,
+      LPCVOID   lpBuffer,
+      DWORD     dwNumberOfBytesToWrite,
+     LPDWORD   lpdwNumberOfBytesWritten
+);
+typedef BOOL(*FnPtrInternetQueryDataAvailable)(
+      HINTERNET hFile,
+     LPDWORD   lpdwNumberOfBytesAvailable,
+      DWORD     dwFlags,
+      DWORD_PTR dwContext
+);
+typedef HINTERNET(*FnPtrInternetOpenUrlW)(
+    HINTERNET hInternet,
+    LPCWSTR   lpszUrl,
+    LPCWSTR   lpszHeaders,
+    DWORD     dwHeadersLength,
+    DWORD     dwFlags,
+    DWORD_PTR dwContext
+);
+typedef BOOL(*FnPtrInternetReadFile)(
+    HINTERNET hFile,
+    LPVOID    lpBuffer,
+    DWORD     dwNumberOfBytesToRead,
+    LPDWORD   lpdwNumberOfBytesRead
+);
+
 
 typedef HWND (*FnPtrCreateWindowA)(
     LPCTSTR lpClassName,
@@ -70,6 +140,16 @@ FnPtrHttpEndRequestW httpEndRequestWPtr;
 FnPtrInternetCloseHandle internetCloseHandlePtr;
 FnPtrHttpSendRequestExW httpSendRequestExWPtr;
 FnPtrInternetConnectW internetConnectWPtr;
+FnPtrHttpQueryInfoW httpQueryInfoWPtr;
+FnPtrHttpAddRequestHeadersW httpAddRequestHeadersWPtr;
+FnPtrHttpSendRequestW httpSendRequestWPtr;
+FnPtrInternetSetOptionW internetSetOptionWPtr;
+FnPtrHttpQueryInfoA httpQueryInfoAPtr;
+FnPtrHttpOpenRequestW httpOpenRequestWPtr;
+FnPtrInternetWriteFile internetWriteFilePtr;
+FnPtrInternetQueryDataAvailable internetQueryDataAvailablePtr;
+FnPtrInternetOpenUrlW internetOpenUrlWPtr;
+FnPtrInternetReadFile internetReadFilePtr;
 
 FnPtrCreateWindowA createWindowAPtr;
 
@@ -144,6 +224,16 @@ VOID AttachHij()
     internetCloseHandlePtr = (FnPtrInternetCloseHandle)GetProcAddress(wininetMod, "InternetCloseHandle");
     httpSendRequestExWPtr = (FnPtrHttpSendRequestExW)GetProcAddress(wininetMod, "HttpSendRequestExW");
     internetConnectWPtr = (FnPtrInternetConnectW)GetProcAddress(wininetMod, "InternetConnectW");
+    httpQueryInfoWPtr = (FnPtrHttpQueryInfoW)GetProcAddress(wininetMod, "HttpQueryInfoW");
+    httpAddRequestHeadersWPtr = (FnPtrHttpAddRequestHeadersW)GetProcAddress(wininetMod, "HttpAddRequestHeadersW");
+    httpSendRequestWPtr=(FnPtrHttpSendRequestW)GetProcAddress(wininetMod, "HttpSendRequestW");
+    internetSetOptionWPtr=(FnPtrInternetSetOptionW)GetProcAddress(wininetMod, "InternetSetOptionW");
+    httpQueryInfoAPtr = (FnPtrHttpQueryInfoA)GetProcAddress(wininetMod, "HttpQueryInfoA");
+    httpOpenRequestWPtr = (FnPtrHttpOpenRequestW)GetProcAddress(wininetMod, "HttpOpenRequestW");
+    internetWriteFilePtr = (FnPtrInternetWriteFile) GetProcAddress(wininetMod, "InternetWriteFile");
+    internetQueryDataAvailablePtr =(FnPtrInternetQueryDataAvailable) GetProcAddress(wininetMod, "InternetQueryDataAvailable");
+    internetOpenUrlWPtr = (FnPtrInternetOpenUrlW)GetProcAddress(wininetMod, "InternetOpenUrlW");
+    internetReadFilePtr = (FnPtrInternetReadFile) GetProcAddress(wininetMod, "InternetReadFile");
 
     DetourTransactionBegin();
     DetourUpdateThread(GetCurrentThread());
@@ -294,4 +384,166 @@ HINTERNET MyInternetConnectW(
         dwService,
         dwFlags,
         dwContext);
+}
+
+BOOL MyHttpQueryInfoW(
+    HINTERNET hRequest,
+    DWORD     dwInfoLevel,
+    LPVOID    lpBuffer,
+    LPDWORD   lpdwBufferLength,
+    LPDWORD   lpdwIndex
+) {
+    Log("[wininet] MyHttpQueryInfoW");
+    return httpQueryInfoWPtr(
+        hRequest,
+        dwInfoLevel,
+        lpBuffer,
+        lpdwBufferLength,
+        lpdwIndex
+    );
+}
+
+BOOL MyHttpAddRequestHeadersW(
+    HINTERNET hRequest,
+    LPCWSTR   lpszHeaders,
+    DWORD     dwHeadersLength,
+    DWORD     dwModifiers
+) {
+    Log("[wininet] MyHttpAddRequestHeadersW");
+    return httpAddRequestHeadersWPtr(
+        hRequest,
+        lpszHeaders,
+        dwHeadersLength,
+        dwModifiers
+    );
+}
+
+
+BOOL MyHttpSendRequestW(
+    HINTERNET hRequest,
+    LPCWSTR   lpszHeaders,
+    DWORD     dwHeadersLength,
+    LPVOID    lpOptional,
+    DWORD     dwOptionalLength
+) {
+    Log("[wininet] MyHttpSendRequestW");
+    return httpSendRequestWPtr(
+        hRequest,
+        lpszHeaders,
+        dwHeadersLength,
+        lpOptional,
+        dwOptionalLength
+    );
+}
+BOOL MyInternetSetOptionW(
+    HINTERNET hInternet,
+    DWORD     dwOption,
+    LPVOID    lpBuffer,
+    DWORD     dwBufferLength
+) {
+    Log("[wininet] MyInternetSetOptionW");
+    return internetSetOptionWPtr(
+        hInternet,
+        dwOption,
+        lpBuffer,
+        dwBufferLength
+    );
+}
+
+
+BOOL MyHttpQueryInfoA(
+    HINTERNET hRequest,
+    DWORD     dwInfoLevel,
+    LPVOID    lpBuffer,
+    LPDWORD   lpdwBufferLength,
+    LPDWORD   lpdwIndex
+) {
+    return httpQueryInfoAPtr(
+        hRequest,
+        dwInfoLevel,
+        lpBuffer,
+        lpdwBufferLength,
+        lpdwIndex
+    );
+}
+HINTERNET MyHttpOpenRequestW(
+    HINTERNET hConnect,
+    LPCWSTR   lpszVerb,
+    LPCWSTR   lpszObjectName,
+    LPCWSTR   lpszVersion,
+    LPCWSTR   lpszReferrer,
+    LPCWSTR* lplpszAcceptTypes,
+    DWORD     dwFlags,
+    DWORD_PTR dwContext
+) {
+    return httpOpenRequestWPtr(
+        hConnect,
+        lpszVerb,
+        lpszObjectName,
+        lpszVersion,
+        lpszReferrer,
+        lplpszAcceptTypes,
+        dwFlags,
+        dwContext
+    );
+}
+
+
+BOOL MyInternetWriteFile(
+    HINTERNET hFile,
+    LPCVOID   lpBuffer,
+    DWORD     dwNumberOfBytesToWrite,
+    LPDWORD   lpdwNumberOfBytesWritten
+) {
+    return internetWriteFilePtr(
+        hFile,
+        lpBuffer,
+        dwNumberOfBytesToWrite,
+        lpdwNumberOfBytesWritten
+    );
+}
+BOOL MyInternetQueryDataAvailable(
+    HINTERNET hFile,
+    LPDWORD   lpdwNumberOfBytesAvailable,
+    DWORD     dwFlags,
+    DWORD_PTR dwContext
+) {
+    return internetQueryDataAvailablePtr(
+        hFile,
+        lpdwNumberOfBytesAvailable,
+        dwFlags,
+        dwContext
+    );
+}
+
+HINTERNET MyInternetOpenUrlW(
+    HINTERNET hInternet,
+    LPCWSTR   lpszUrl,
+    LPCWSTR   lpszHeaders,
+    DWORD     dwHeadersLength,
+    DWORD     dwFlags,
+    DWORD_PTR dwContext
+) {
+    return internetOpenUrlWPtr(
+        hInternet,
+        lpszUrl,
+        lpszHeaders,
+        dwHeadersLength,
+        dwFlags,
+        dwContext
+    );
+}
+
+BOOL MyInternetReadFile(
+    HINTERNET hFile,
+    LPVOID    lpBuffer,
+    DWORD     dwNumberOfBytesToRead,
+    LPDWORD   lpdwNumberOfBytesRead
+) {
+    return internetReadFilePtr(
+        hFile,
+        lpBuffer,
+        dwNumberOfBytesToRead,
+        lpdwNumberOfBytesRead
+    );
 }
