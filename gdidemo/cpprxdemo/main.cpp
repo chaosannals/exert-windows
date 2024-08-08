1f4d6e0a99b239c907722bdd210b834a7e4ec37c
chaosannals <chaosannals@outlook.com>
2024-08-08 15:28:27 +0800

    save.

--- gdidemo/cpprxdemo/main.cpp
+++ gdidemo/cpprxdemo/main.cpp
@@ -2,27 +2,24 @@
 #include <rxcpp/rx.hpp>
 
 int main() {
-    auto keys = rxcpp::observable<>::create<int>(
-    [](rxcpp::subscriber<int> dest) {
+    auto keys = rxcpp::observable<>::create<int>([](rxcpp::subscriber<int> dest) {
         for (;;) {
             int key = std::cin.get();
             dest.on_next(key);
         }
-    }).
-    publish();
+    }).publish();
 
-    auto a = keys.
-        filter([](int key) {return std::tolower(key) == 'a'; });
+    auto a = keys.filter([](int key) {return std::tolower(key) == 'a'; });
 
-    auto g = keys.
-        filter([](int key) {return std::tolower(key) == 'g'; });
+    auto g = keys.filter([](int key) {return std::tolower(key) == 'g'; });
 
-    a.merge(g).
-        subscribe([](int key) {
+    a.merge(g).subscribe([](int key) {
         std::cout << key << std::endl;
     });
 
     // run the loop in create
     keys.connect();
+
+    std::cout << "END" << std::endl;
     return 0;
 }
