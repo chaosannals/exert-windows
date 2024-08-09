e7df92143b1a2b322c71207a65f40cccd06a20df
chaosannals <chaosannals@outlook.com>
2024-08-09 16:58:59 +0800

    save.

---
+++ gdidemo/gdipdemo/wnd.cpp
@@ -0,0 +1,82 @@
+﻿#include "wnd.h"
+
+#include <objidl.h>
+#include <gdiplus.h>
+
+TCHAR szWindowClass[] = TEXT("GDI+ DEMO STAR WND CLASS NAME");
+TCHAR szTitle[] = TEXT("GDI+ Demo Simple");
+
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+        break;
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+
+        Gdiplus::Graphics g(hdc);
+
+        Gdiplus::Pen pen(Gdiplus::Color(255, 255, 0, 255));
+        g.DrawRectangle(&pen, 10, 10, 200, 100);
+        g.DrawLine(&pen, 0, 0, 200, 100);
+
+        Gdiplus::FontFamily  fontFamily(L"Times New Roman");
+        Gdiplus::Font        font(&fontFamily, 24, Gdiplus::FontStyleRegular, Gdiplus::UnitPixel);
+        Gdiplus::PointF      pointF(30.0f, 10.0f);
+        Gdiplus::SolidBrush  solidBrush(Gdiplus::Color(255, 0, 0, 255));
+
+        g.DrawString(L"Hello中文", -1, &font, pointF, &solidBrush);
+
+        EndPaint(hWnd, &ps);
+        break;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return S_OK;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEX wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    //wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_HelloStar));
+    wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    //wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_HelloStar);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = szWindowClass;
+    //wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+    return RegisterClassEx(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    HWND hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, 240, 320, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+    return TRUE;
+}
